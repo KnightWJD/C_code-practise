c9816fbad4a45d57af3980f68bc496b81921b2f0
KnightWJD <2630225285@qq.com>
2021-01-10 17:58:57 +0800

    strcmp

---
+++ welcome to China! ^-^_strcmp.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h> //strlen的头文件
+#include <windows.h> //sleep头文件
+#include <stdlib.h> //system 的头文件
+int main()
+{
+    char arr1[] = {"welcome to China! ^-^"};
+    char arr2[] = {"#####################"};
+    int left = 0;
+//    int right = sizeof(arr1) / sizeof(arr[0]) -2; // -2是因为字符串有一个\n
+    int right = strlen(arr1)-1;
+    while(right>=left)
+    {
+        arr2[left]=arr1[left];
+        arr2[right]=arr1[right];
+
+        left++;
+        right--;
+
+        printf("%s\n",arr2);
+        if(strcmp(arr1,arr2) == 0)
+        {
+            continue;
+        }
+        Sleep(1000);//休息1s
+        system("cls"); //system-执行系统命令的一个函数 cls-清空屏幕
+
+    }
+    //printf("%s\n",arr2);
+
+    return 0;
+}
