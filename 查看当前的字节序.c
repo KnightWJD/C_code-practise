bb059804fb38d8673d63e3ee551c058a0d44a5c1
knightWJD <2630225285@qq.com>
2021-03-18 10:19:33 +0800

    *(char*)&a

---
+++ 查看当前的字节序.c
@@ -0,0 +1,25 @@
+﻿//写一段代码,查看当前的字节序是多少
+
+#include <stdio.h>
+
+int check_sys()
+{
+    //返回1为小端,返回0为大端
+    int a = 1;
+    return *(char*)&a;
+}
+
+int main()
+{
+    int ret = check_sys();
+    if(ret == 0)
+    {
+        printf("大端\n");
+    }
+    if(ret == 1)
+    {
+        printf("小端\n");
+    }
+    return 0;
+}
+
